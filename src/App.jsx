c9b643e4f3a321645d1e72aeac2adf2081dcf8c1
Russell <russell.xu@techfi.io>
2022-10-05 21:39:44 +0800

    add input & input css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ const App = () => {
     <>
       <h1>FAIR GAME</h1>
       <button>Connect</button>
+      <h2>Balance: 26.5786532</h2>
+      <input type="number" />
+      <button>Deposit</button>
+      <input type="number" />
+      <button>Withdrawal</button>
+      <input type="number" />
+      <button>Bet</button>
     </>
   );
 };
