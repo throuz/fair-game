3ca268ddefcd0223eb8b736edb28c5ca1ec88fe1
Russell <russell.xu@techfi.io>
2022-10-05 23:45:32 +0800

    add placeholder & optimize balance display

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ const App = () => {
       <h1>FAIR GAME</h1>
       <div className="card">
         <button>Connect</button>
-        <h2>Balance: 26.5786532</h2>
-        <input type="number" />
+        <h2>26.5786532 BNB</h2>
+        <input type="number" placeholder="Please enter deposit amount" />
         <button>Deposit</button>
-        <input type="number" />
+        <input type="number" placeholder="Please enter withdrawal amount" />
         <button>Withdrawal</button>
       </div>
       <div className="card">
-        <input type="number" />
+        <input type="number" placeholder="Please enter bet amount" />
         <button>Bet</button>
       </div>
     </div>
