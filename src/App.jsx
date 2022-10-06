e7698a02d9303fc3826fc1ef8fd326f27cc6c636
Russell <russell.xu@techfi.io>
2022-10-06 23:26:05 +0800

    add card title

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ const App = () => {
     <div className="app">
       <h1>FAIR GAME</h1>
       <div className="card">
+        <h2>Information</h2>
         <button>Connect</button>
-        <h2>26.5786532 BNB</h2>
+        <h2>26.57865327 BNB</h2>
         <input type="number" placeholder="Please enter deposit amount" />
         <button>Deposit</button>
         <input type="number" placeholder="Please enter withdrawal amount" />
         <button>Withdrawal</button>
       </div>
       <div className="card">
+        <h2>Game</h2>
         <input type="number" placeholder="Please enter bet amount" />
         <button>Bet</button>
       </div>
