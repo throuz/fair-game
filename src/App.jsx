a90d8a7e14156b1e8e9c61fd3cdb3d768ac73488
Russell <russell.xu@techfi.io>
2022-10-07 17:58:24 +0800

    add How to play

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,10 @@ const App = () => {
         <button>Withdrawal</button>
       </div>
       <div className="card">
-        <h2>Game</h2>
+        <h2>Bet</h2>
+        <span>Please choose a strategy to auto bet or bet manually</span>
+        <button>Martingale</button>
+        <button>Anti-Martingale</button>
         <input type="number" placeholder="Please enter bet amount" />
         <button>Bet</button>
       </div>
@@ -62,6 +65,105 @@ const App = () => {
             <span>12:10:13</span>
           </div>
         </div>
+      </div>
+      <div className="card">
+        <h2>How to play</h2>
+        <h3>Manual Bet</h3>
+        <span className="text-left">
+          Enter the desired bet amount and click the bet button.
+        </span>
+        <h3>Auto Bet</h3>
+        <span className="text-left">
+          Choose a strategy and enter the bet amount, then click the bet button.
+          <br />
+          There are currently two strategies to choose from:
+        </span>
+        <h4>Martingale:</h4>
+        <span className="text-left">
+          If win, next bet amount is initial bet amount, if lose, next bet is
+          double previous bet amount.
+        </span>
+        <span>E.g: Initial bet amount is 0.01 ETH</span>
+        <table>
+          <tr>
+            <th>Status</th>
+            <th>Amount</th>
+            <th>Next Amount</th>
+          </tr>
+          <tr>
+            <td>Win</td>
+            <td>0.01</td>
+            <td>0.01</td>
+          </tr>
+          <tr>
+            <td>Lose</td>
+            <td>0.01</td>
+            <td>0.02</td>
+          </tr>
+          <tr>
+            <td>Lose</td>
+            <td>0.02</td>
+            <td>0.04</td>
+          </tr>
+          <tr>
+            <td>Lose</td>
+            <td>0.04</td>
+            <td>0.08</td>
+          </tr>
+          <tr>
+            <td>Win</td>
+            <td>0.08</td>
+            <td>0.01</td>
+          </tr>
+          <tr>
+            <td>Win</td>
+            <td>0.01</td>
+            <td>0.01</td>
+          </tr>
+        </table>
+        <h4>Anti-Martingale:</h4>
+        <span className="text-left">
+          If win, next bet amount is double previous bet amount, if lose, next
+          bet is initial bet amount.
+        </span>
+        <span>E.g: Initial bet amount is 0.01 ETH</span>
+        <table>
+          <tr>
+            <th>Status</th>
+            <th>Amount</th>
+            <th>Next Amount</th>
+          </tr>
+          <tr>
+            <td>Win</td>
+            <td>0.01</td>
+            <td>0.02</td>
+          </tr>
+          <tr>
+            <td>Lose</td>
+            <td>0.02</td>
+            <td>0.01</td>
+          </tr>
+          <tr>
+            <td>Lose</td>
+            <td>0.01</td>
+            <td>0.01</td>
+          </tr>
+          <tr>
+            <td>Win</td>
+            <td>0.01</td>
+            <td>0.02</td>
+          </tr>
+          <tr>
+            <td>Win</td>
+            <td>0.02</td>
+            <td>0.04</td>
+          </tr>
+          <tr>
+            <td>Win</td>
+            <td>0.04</td>
+            <td>0.08</td>
+          </tr>
+        </table>
       </div>
     </div>
   );
