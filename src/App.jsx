53d5d0362e036b1067b4d669ff4e786d0011e89d
Russell <russell.xu@techfi.io>
2022-10-11 16:09:30 +0800

    deposit and withdrawal button layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Game from "./components/Game";
 import History from "./components/History";
 import HowToPlay from "./components/HowToPlay";
-import Infomation from "./components/Infomation";
+import Information from "./components/Information";
 
 const App = () => {
   return (
     <div className="app">
       <h1 className="title">FAIR GAME</h1>
       <div className="container">
-        <Infomation />
+        <Information />
         <Game />
         <History />
         <HowToPlay />
---
+++ src/components/Information.jsx
@@ -0,0 +1,18 @@
+import "./Information.css";
+
+const Information = () => {
+  return (
+    <div className="card">
+      <h2>Information</h2>
+      <button>Connect</button>
+      <h2>26.57865327 BNB</h2>
+      <input type="number" placeholder="Please enter amount" />
+      <div className="information-btn-group">
+        <button>Deposit</button>
+        <button>Withdrawal</button>
+      </div>
+    </div>
+  );
+};
+
+export default Information;
