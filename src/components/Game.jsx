090e87cd2e7ce39ae70c9ef611faa5eac2b4bd72
Russell <russell.xu@techfi.io>
2022-10-11 15:16:33 +0800

    strategy button style

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,36 @@
+import "./Game.css";
+import { useState } from "react";
+
 const Game = () => {
+  const [strategy, setStrategy] = useState("noStrategy");
+
   return (
     <div className="card">
       <h2>Game</h2>
-      <button>No Strategy</button>
-      <button>Martingale</button>
-      <button>Anti-Martingale</button>
+      <button
+        className={`strategy-btn ${
+          strategy === "noStrategy" ? "active-strategy-btn" : ""
+        }`}
+        onClick={() => setStrategy("noStrategy")}
+      >
+        No Strategy
+      </button>
+      <button
+        className={`strategy-btn ${
+          strategy === "martingale" ? "active-strategy-btn" : ""
+        }`}
+        onClick={() => setStrategy("martingale")}
+      >
+        Martingale
+      </button>
+      <button
+        className={`strategy-btn ${
+          strategy === "antiMartingale" ? "active-strategy-btn" : ""
+        }`}
+        onClick={() => setStrategy("antiMartingale")}
+      >
+        Anti-Martingale
+      </button>
       <h2>--</h2>
       <input type="number" placeholder="Please enter bet amount" />
       <button>Bet</button>
