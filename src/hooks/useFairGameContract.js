bbd0517e884ea1e3c6b341dfc730211aaceae223
Russell <russell.xu@techfi.io>
2022-10-16 14:31:56 +0800

    add contract hook

---
+++ src/hooks/useFairGameContract.js
@@ -0,0 +1,18 @@
+import { abi } from "../utils/FairGame.json";
+
+const useFairGameContract = () => {
+  const { ethereum } = window;
+  if (ethereum && ethereum.isMetaMask) {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const fairGameContract = new ethers.Contract(
+      "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      abi,
+      signer
+    );
+    return fairGameContract;
+  }
+  return null;
+};
+
+export default useFairGameContract;
