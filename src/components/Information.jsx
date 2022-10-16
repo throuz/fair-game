86a9382d8e3e82ce1d26c0fc5de415a54666925c
Russell <russell.xu@techfi.io>
2022-10-16 20:51:56 +0800

    deposit and withdrawal feature

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,17 +1,73 @@
 import "./Information.css";
+import { ethers } from "ethers";
+import { StoreContext } from "../store";
+import { useContext, useState } from "react";
 import Balance from "./Balance";
 import ConnectButton from "./ConnectButton";
+import useFairGameContract from "../hooks/useFairGameContract";
 
 const Information = () => {
+  const { store } = useContext(StoreContext);
+  const { accountStatus } = store;
+  const fairGameContract = useFairGameContract();
+  const [amount, setAmount] = useState("");
+  const [isAmountValid, setIsAmountValid] = useState(true);
+
+  const onAmountInputChange = (e) => {
+    const newAmount = e.target.value;
+    setIsAmountValid(newAmount > 0 && /^[0-9]+(.[0-9]{0,8})?$/.test(newAmount));
+    setAmount(newAmount);
+  };
+
+  const onDepositClick = async () => {
+    try {
+      if (accountStatus === "connected") {
+        if (isAmountValid) {
+          const depositTxn = await fairGameContract.deposit({
+            value: ethers.utils.parseEther(amount),
+          });
+          await depositTxn.wait();
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onWithdrawalClick = async () => {
+    try {
+      if (accountStatus === "connected") {
+        if (isAmountValid) {
+          const withdrawalTxn = await fairGameContract.withdrawal(
+            ethers.utils.parseEther(amount)
+          );
+          await withdrawalTxn.wait();
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="card">
       <h2>Information</h2>
       <ConnectButton />
       <Balance />
-      <input type="number" placeholder="Please enter amount" />
+      <div>
+        <input
+          type="number"
+          placeholder="Please enter amount"
+          value={amount}
+          onChange={onAmountInputChange}
+        />
+        {!isAmountValid && (
+          <div className="input-error-msg">Please enter a valid amount</div>
+        )}
+      </div>
       <div className="information-btn-group">
-        <button>Deposit</button>
-        <button>Withdrawal</button>
+        <button onClick={onDepositClick}>Deposit</button>
+        <button onClick={onWithdrawalClick}>Withdrawal</button>
       </div>
     </div>
   );
