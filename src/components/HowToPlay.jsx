a84fabe5eb66f7c874ad8b0d173af43a02df55b6
Russell <russell.xu@techfi.io>
2022-10-25 18:00:51 +0800

    fix how to play

--- src/components/HowToPlay.jsx
+++ src/components/HowToPlay.jsx
@@ -3,19 +3,21 @@ const HowToPlay = () => {
     <div className="card">
       <h2>How to play</h2>
       <span className="text-left">
-        Enter the desired bet amount and click the bet button, if the number is
-        greater than 50, is win and you will get double bet amount. Every bet
-        has a 50% chance of winning. There are three strategies to choose from:
+        Enter the desired bet amount and click the bet button, every bet has a
+        50% chance of winning.
+      </span>
+      <span className="text-left">
+        There are three strategies to choose from:
       </span>
       <h3>No Strategy</h3>
-      <span>Manual betting, required for each bet.</span>
+      <span className="text-left">Manual betting, required for each bet.</span>
       <h3>Martingale</h3>
       <span className="text-left">
         Automatic betting, which can be stopped at any time. If win, next bet
-        amount is initial bet amount, if lose, next bet is double previous bet
-        amount.
+        amount is initial bet amount, if lose, next bet amount is double
+        previous bet amount.
       </span>
-      <span>E.g: Initial bet amount is 0.01 ETH</span>
+      <span>E.g: Initial bet amount is 0.01 BNB</span>
       <table>
         <thead>
           <tr>
@@ -60,10 +62,10 @@ const HowToPlay = () => {
       <h3>Anti-Martingale</h3>
       <span className="text-left">
         Automatic betting, which can be stopped at any time. If win, next bet
-        amount is double previous bet amount, if lose, next bet is initial bet
-        amount.
+        amount is double previous bet amount, if lose, next bet amount is
+        initial bet amount.
       </span>
-      <span>E.g: Initial bet amount is 0.01 ETH</span>
+      <span>E.g: Initial bet amount is 0.01 BNB</span>
       <table>
         <thead>
           <tr>
