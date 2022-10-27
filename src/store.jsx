7e10cad7a74708bc6466525c375174378a72fcf0
Russell <russell.xu@techfi.io>
2022-10-27 18:52:23 +0800

    switch & add network

--- src/store.jsx
+++ src/store.jsx
@@ -19,36 +19,77 @@ export default ({ children }) => {
   });
 
   useEffect(() => {
+    const errorHandle = (error) => {
+      if (error.reason) {
+        setStore({
+          ...store,
+          status: "notConnected",
+          modalShow: true,
+          modalText: capitalizeFirstLetter(error.reason),
+        });
+      } else {
+        setStore({
+          ...store,
+          status: "notConnected",
+          modalShow: true,
+          modalText: "Unknown error",
+        });
+      }
+    };
+
     (async () => {
       try {
         const { ethereum } = window;
         if (ethereum && ethereum.isMetaMask) {
           setStore({ ...store, status: "notConnected" });
-          const accounts = await ethereum.request({ method: "eth_accounts" });
-          if (accounts[0]) {
-            const userBalance = await fairGameContract.users(accounts[0]);
-            setStore({
-              ...store,
-              status: "connected",
-              address: accounts[0],
-              balance: formatEther(userBalance),
-            });
+          if (ethereum.chainId !== "0x7a69") {
+            try {
+              await ethereum.request({
+                method: "wallet_switchEthereumChain",
+                params: [{ chainId: "0x7a69" }],
+              });
+              location.reload();
+            } catch (error) {
+              if (error.code === 4902) {
+                try {
+                  await ethereum.request({
+                    method: "wallet_addEthereumChain",
+                    params: [
+                      {
+                        chainId: "0x7a69",
+                        chainName: "Localhost network",
+                        rpcUrls: ["http://127.0.0.1:8545/"],
+                        nativeCurrency: {
+                          name: "Localhost BNB",
+                          symbol: "BNB",
+                          decimals: 18,
+                        },
+                      },
+                    ],
+                  });
+                  location.reload();
+                } catch (error) {
+                  errorHandle(error);
+                }
+              } else {
+                errorHandle(error);
+              }
+            }
+          } else {
+            const accounts = await ethereum.request({ method: "eth_accounts" });
+            if (accounts[0]) {
+              const userBalance = await fairGameContract.users(accounts[0]);
+              setStore({
+                ...store,
+                status: "connected",
+                address: accounts[0],
+                balance: formatEther(userBalance),
+              });
+            }
           }
         }
       } catch (error) {
-        if (error.reason) {
-          setStore({
-            ...store,
-            modalShow: true,
-            modalText: error.reason,
-          });
-        } else {
-          setStore({
-            ...store,
-            modalShow: true,
-            modalText: "Unknown error",
-          });
-        }
+        errorHandle(error);
       }
     })();
   }, []);
