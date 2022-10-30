20e1c58daeba27ad623c971bfaadf1384e1d9a0a
Russell <russell.xu@techfi.io>
2022-10-30 13:15:05 +0800

    add production contract address

--- src/utils/contractAddress.js
+++ src/utils/contractAddress.js
@@ -1,6 +1,6 @@
 const contractAddressMap = {
   development: "0x635E0B6b334BFBaE18D0F346C2D99D55078422b0",
-  production: "",
+  production: "0x73E8bE702063532dE9850167354d161D5950C57e",
 };
 
 export default contractAddressMap[import.meta.env.MODE];
