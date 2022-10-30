0d73a0ccef7727e96fc46a7abe81019f65302393
Russell <russell.xu@techfi.io>
2022-10-30 12:10:55 +0800

    add sub title & contract address

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import contractAddress from "./utils/contractAddress";
 import Game from "./components/Game";
 import History from "./components/History";
 import HowToPlay from "./components/HowToPlay";
 import Information from "./components/Information";
 
 const App = () => {
+  const onContractAddressClick = () => {
+    window
+      .open(`https://bscscan.com/address/${contractAddress}`, "_blank")
+      .focus();
+  };
+
   return (
     <div className="app">
-      <h1 className="title">FAIR GAME</h1>
+      <div className="header">
+        <h1 className="header-title">FAIR GAME</h1>
+        <h3 className="header-subtitle">
+          50% chance of 100% reward on every bet
+        </h3>
+        <button className="contract-address" onClick={onContractAddressClick}>
+          {contractAddress}
+        </button>
+      </div>
       <div className="container">
         <Information />
         <Game />
