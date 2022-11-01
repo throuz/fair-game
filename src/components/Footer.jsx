cf8f770e8dfefcf9204ac48e4ac58777387bc4a6
Russell <russell.xu@techfi.io>
2022-11-01 15:58:24 +0800

    add social media direct

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
 import "./Footer.css";
 
 const Footer = () => {
+  const onTelegramClick = () => {
+    window.open("https://t.me/+En-AsBSMqeRlZmU1", "_blank").focus();
+  };
+
+  const onYoutubeClick = () => {
+    window
+      .open(
+        "https://www.youtube.com/channel/UCb6hxQuHEWPaA6dlfRp7fhg",
+        "_blank"
+      )
+      .focus();
+  };
+
   return (
     <footer>
       <div className="footer-btn-group">
-        <button className="footer-btn">TELEGRAM</button>
-        <button className="footer-btn">YOUTUBE</button>
+        <button className="footer-btn" onClick={onTelegramClick}>
+          TELEGRAM
+        </button>
+        <button className="footer-btn" onClick={onYoutubeClick}>
+          YOUTUBE
+        </button>
       </div>
       <h4>Copyright © 2022 Throuz</h4>
     </footer>
